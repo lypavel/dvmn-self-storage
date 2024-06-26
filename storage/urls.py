1f35091463944e0f8f0a8c62629fcd2df1378a90
lypavel <157053921+lypavel@users.noreply.github.com>
2024-06-26 14:24:39 +0300

    Connect boxes.html to site and base.html

--- storage/urls.py
+++ storage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from storage.views import index, faq
+from storage.views import boxes, index, faq
 
 app_name = 'storage'
 
 urlpatterns = [
     path('', index, name='index'),
     path('faq', faq, name='faq'),
+    path('boxes', boxes, name='boxes')
 ]
--- storage/views.py
+++ storage/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def faq(request):
     return render(request, 'storage/faq.html')
+
+
+def boxes(request):
+    return render(request, 'storage/boxes.html')
